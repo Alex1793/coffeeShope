fc17cc74e527d986aae320be36cda38309625995
Alexander <sasha.zhulega.02@mail.ru>
2023-10-31 22:55:26 +0300

    start verstka

---
+++ src/components/mainPage/mainPage.js
@@ -0,0 +1,37 @@
+import './mainPage.sass'
+
+function MainPage () {
+    return (
+        <div>
+            <section className="header">
+                <h1 className="header_descr">Everything You Love About Coffee</h1>
+
+                <div className="header_text">
+                    We makes every day full of energy and taste
+                    <br />
+                    Want to try our beans?
+                </div>
+
+
+                <button className="header_btn">More</button>
+            </section>
+
+            <section className="about">
+                <div className="about_header">About Us</div>
+                <div className="about_descr">Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
+                Afraid at highly months do things on at. Situation recommend objection do intention
+                so questions. As greatly removed calling pleased improve an. Last ask him cold feel
+                met spot shy want. Children me laughing we prospect answered followed. At it went
+                is song that held help face.
+                <br /> <br />
+                Now residence dashwoods she excellent you. Shade being under his bed her, Much
+                read on as draw. Blessing for ignorant exercise any yourself unpacked. Pleasant
+                horrible but confined day end marriage. Eagerness furniture set preserved far
+                recommend. Did even but nor are most gave hope. Secure active living depend son
+                repair day ladies now.</div>
+            </section>
+        </div>
+    )
+}
+
+export default MainPage;
